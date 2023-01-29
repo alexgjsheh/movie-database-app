0d2cda4f094ef203eb661776fc37e33481d00f6f
Alexander Sheh <alexgjsheh@gmail.com>
2023-01-29 15:22:39 -0800

    Updated CSS and add more movies button

---
+++ src/components/MoreMoviesBtn.js
@@ -0,0 +1,9 @@
+function MoreMoviesBtn({ onClick }) {
+    return (
+        <button className="more-movies-btn" onClick={onClick}>
+            More Movies
+        </button>
+    );
+}
+
+export default MoreMoviesBtn;
