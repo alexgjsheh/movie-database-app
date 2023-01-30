bebe324534129c32d2d355253f848b63f715e3fb
Alexander Sheh <alexgjsheh@gmail.com>
2023-01-29 20:51:03 -0800

    Updated Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="footer">
             <div>
-                <p>&copy; 2023 Alex Sheh</p>
+                <p>&copy; 2023 Alexander Sheh & Peter Nguyen</p>
             </div>
         </footer>
     );
