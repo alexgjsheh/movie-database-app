b9b1b8c1a790ce224df9f6d24dff5b8ad25df782
Alexander Sheh <alexgjsheh@gmail.com>
2023-01-29 21:05:48 -0800

    Added class and styling to h2 on favorites page

--- src/pages/PageFavorites.js
+++ src/pages/PageFavorites.js
@@ -3,42 +3,40 @@ import Card from "../components/Card";
 import { useSelector } from "react-redux";
 
 function PageFavorites() {
-  const favs = useSelector((state) => state.favs.items);
+    const favs = useSelector((state) => state.favs.items);
 
-  useEffect(() => {
-    document.title = `filmsPerSecond - Favorites`;
-  }, []);
+    useEffect(() => {
+        document.title = `filmsPerSecond - Favorites`;
+    }, []);
 
-  return (
-    <main>
-      <section>
-        <h2>Favourite Movies</h2>
-        {favs.length < 1 ? (
-          <p>
-            No favorite Movies. Please add some favorite Movies.
-          </p>
-        ) : (
-          <div className="movie-card-container">
-            {favs.map((movie, i) => {
-              return (
-                <Card
-                object={favs[i]}
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                voteAverage={movie.vote_average}
-                overview={movie.overview}
-                posterPath={movie.poster_path}
-                releaseDate={movie.release_date}
-                isFav={true}
-                />
-              );
-            })}
-          </div>
-          )}
-      </section>
-    </main>
-  );
+    return (
+        <main>
+            <section className="main-content">
+                <h2 className="favorite-movies-heading">Favorite Movies</h2>
+                {favs.length < 1 ? (
+                    <p>No favorite Movies. Please add some favorite Movies.</p>
+                ) : (
+                    <div className="movie-card-container">
+                        {favs.map((movie, i) => {
+                            return (
+                                <Card
+                                    object={favs[i]}
+                                    key={movie.id}
+                                    id={movie.id}
+                                    title={movie.title}
+                                    voteAverage={movie.vote_average}
+                                    overview={movie.overview}
+                                    posterPath={movie.poster_path}
+                                    releaseDate={movie.release_date}
+                                    isFav={true}
+                                />
+                            );
+                        })}
+                    </div>
+                )}
+            </section>
+        </main>
+    );
 }
 
 export default PageFavorites;
