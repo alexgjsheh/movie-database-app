12b322de37a2604a316d5a47ae47829bd232eace
Alexander Sheh <alexgjsheh@gmail.com>
2023-01-31 01:32:55 -0800

    Updated PageNotFound page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -4,7 +4,17 @@ function PageNotFound() {
     useEffect(() => {
         document.title = `filmsPerSecond - Page Not Found`;
     }, []);
-    return <h1>Page Not Found</h1>;
+    return (
+        <>
+            <section className="page-not-found-section">
+                <h2 className="page-not-found-heading">Page Not Found</h2>
+                <p>
+                    The page you are looking for was not found. Please go back
+                    to home.
+                </p>
+            </section>
+        </>
+    );
 }
 
 export default PageNotFound;
