a577eef3fafa259df76615fa2638e61984f61348
Alexander Sheh <alexgjsheh@gmail.com>
2023-02-03 22:13:27 -0800

    Added rating color class for home page cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -91,7 +91,9 @@ function Card({
             <div className="movie-info-container">
                 <p className="movie-title">{title}</p>
                 <p>{releaseDate}</p>
-                <p>{`${Math.round(voteAverage * 10)}%`}</p>
+                <p className={voteAverage < 5 ? "red" : ""}>{`${Math.round(
+                    voteAverage * 10
+                )}%`}</p>
             </div>
         </article>
     );
